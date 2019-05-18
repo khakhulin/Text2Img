99dd638a8435c304e9feef23bfe40332cb550fcb
khakhulin <t.khakhulin@gmail.com>
2019-05-18 23:51:18 +0300

    add dataset downloading scripts for birds

---
+++ scripts/download_birds_data.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+wget http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz
+tar -xvf CUB_200_2011.tgz CUB_200_2011.tgz CUB_200_2011/images
+tar -xvf CUB_200_2011.tgz CUB_200_2011/images.txt
+curl -L "https://drive.google.com/uc?export=download&id=1HXnzREyrcYkJiF00rIsyjgA6X3ljPK40" > labels.tar.gz
+tar -xvf labels.tar.gz
+mkdir -p datasets/CUB_200_2011/
+mv CUB_200_2011/images datasets/CUB_200_2011/images
+mv CUB_200_2011/images.txt datasets/CUB_200_2011/
+mv text_c10 datasets/CUB_200_2011/text_c10
+rm -rf CUB_200_2011
