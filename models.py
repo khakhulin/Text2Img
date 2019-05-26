Fixed bug in generator (GLU applied to a wrong axis)

models.py
@@ -9,11 +9,10 @@ class ResBlock(nn.Module):
         self.block = nn.Sequential(
             conv3x3(channel_num, channel_num * 2),
             nn.BatchNorm2d(channel_num * 2),
-            nn.GLU(),
-            ReViewLastDim(),
+            nn.GLU(dim=1),
             conv3x3(channel_num, channel_num),
             nn.BatchNorm2d(channel_num),
-            )
+        )
 
 
     def forward(self, x):
@@ -96,7 +95,8 @@ class UpGenMode(nn.Module):
         self.fc = nn.Sequential(
             nn.Linear(nz, ngf * 4 * 4 * 2, bias=False),
             nn.BatchNorm1d(ngf * 4 * 4 * 2),
-            nn.GLU())
+            nn.GLU()
+        )
 
         self.upsample = nn.Sequential(
             UpBlock(ngf, ngf // 2),

nn_utils.py
@@ -31,32 +31,25 @@ class Interpolate(nn.Module):
         return x
 
 
-class ReViewLastDim(nn.Module):
-    def forward(self, x):
-        a,b,c,d, = x.size()
-        return x.view(a, b//2 , c, 2*d)
-
-
 class UpBlock(nn.Sequential):
     def __init__(self, in_planes, out_planes):
         super().__init__(nn.Sequential(
             Interpolate(scale_factor=2, mode='nearest'),
             conv3x3(in_planes, out_planes * 2),
             nn.BatchNorm2d(out_planes * 2),
-            nn.GLU(),
-            ReViewLastDim()
+            nn.GLU(dim=1)
         ))
 
 
 # Keep the spatial size
 
-class GluConv(nn.Sequential):
-    def __init__(self, in_planes, out_planes):
-        super().__init__(nn.Sequential(
-            conv3x3(in_planes, out_planes * 2),
-            nn.BatchNorm2d(out_planes * 2),
-            nn.GLU()
-        ))
+# class GluConv(nn.Sequential):
+#     def __init__(self, in_planes, out_planes):
+#         super().__init__(nn.Sequential(
+#             conv3x3(in_planes, out_planes * 2),
+#             nn.BatchNorm2d(out_planes * 2),
+#             nn.GLU()
+#         ))
 
 
 class LeakyConv3x3(nn.Sequential):