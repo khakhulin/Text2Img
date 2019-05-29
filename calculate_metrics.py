279ec87d7f7decdb883a825c18f68949cfbaa0ef
SemyonAbramov <semyon.abramov.mipt@gmail.com>
2019-05-29 13:51:33 +0300

    Added calculation of inception score

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -5,6 +5,12 @@ from scores.prd_score import prd_score
 from text2img_model import Text2ImgModel
 from data_utils import BirdsPreprocessor, CaptionTokenizer, BirdsDataset, prepare_data
 from torch.utils.data import DataLoader
+import tqdm
+import os
+import time
+import datetime
+import torch
+from utils import save_images
 
 class Text2ImgTester():
 	def __init__(self, data_path, batch_size, embd_size, text_enc_emb_size, pretrained_text_enc, pretrained_image_enc, pretrained_generator, branch_num, is_bert, base_size, device, use_sagan):
@@ -13,7 +19,7 @@ class Text2ImgTester():
 		print ("self dataset: ", self.dataset)
 		self.batch_size = batch_size
 		self.data_loader = DataLoader(dataset=self.dataset, batch_size=self.batch_size, shuffle=True, drop_last=True)
-
+		self.device = device
 
 		self.model = Text2ImgModel(
 			embedding_dim=embd_size,
@@ -41,14 +47,45 @@ class Text2ImgTester():
 
 		return dataset
 
-	def get_inception_score(path_to_generated_imgs):
+	def get_inception_score(self, path_to_generated_imgs):
 		gen_img_iterator = GenImgData(gen_save_folder)
 		mean_val, std_val = inception_score(gen_img_iterator, cuda=False, batch_size=32, resize=False, splits=1)
 
 		return mean_val, std_val
 
-		#def get_fid_score(path_to_generated_imgs, path_to_real_imgs):
-		#	pass
+	def get_scores(self):
+		print ("len dataloader: ", len(self.data_loader))
+
+		cur_time = datetime.datetime.now().strftime('%d:%m:%Y:%H-%M-%S')
+		run_name = os.path.join('gen_exp', cur_time)
+		save_dir = os.path.join('generated_images', run_name)
+		
+		self.model.generator.eval()
+
+		gen_iter = 0 
+
+		for data in tqdm.tqdm(self.data_loader, total=len(self.data_loader)):
+			images, captions, cap_lens, masks, class_ids = prepare_data(data, self.device)
+			noise = torch.FloatTensor(
+				captions.size(0),
+				self.model.z_dim
+				).to(self.device).normal_(0, 1)
+
+			gen_iter += 1
+			gen_images, _, _, _, _ = self.model(captions, cap_lens, noise, masks)
+			filenames = [str(gen_iter) + str(i) for i in range(gen_images[-1].size(0))]
+			img_tensor = save_images(gen_images[-1], filenames, save_dir, '', gen_images[-1].size(3))
+
+
+		gen_save_folder = os.path.join(save_dir, 'images', 'iter', str(gen_images[-1].size(3)))
+		gen_img_iterator = GenImgData(gen_save_folder)
+		mean_val, std_val = inception_score(gen_img_iterator, cuda=False, batch_size=32, resize=False, splits=4)
+		print ("inception score")
+		print ("mean: ", mean_val)
+		print ("std: ", std_val)
+
+
+
 
 if __name__ == '__main__':
 	
@@ -65,5 +102,8 @@ if __name__ == '__main__':
 			base_size=64,
 			device='cpu',
 			use_sagan=False)
+
+	model.get_scores()
+
 
 
